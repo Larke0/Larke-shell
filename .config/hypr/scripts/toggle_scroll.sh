c9901f2ed8b11083e7a8bb15612386d559f6ea5b
Larke <larke850@gmail.com>
2026-05-01 16:38:00 -0300

    fix toggle_scroll auto-restore

--- .config/hypr/scripts/toggle_scroll.sh
+++ .config/hypr/scripts/toggle_scroll.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 if [ "$1" == "off" ]; then
-    hyprctl keyword input:scroll_factor 0
+  hyprctl keyword input:scroll_factor 0
+
+  # Auto-restore after 2 seconds no matter what
+  (sleep 5 && hyprctl keyword input:scroll_factor 1) &
 else
-    hyprctl keyword input:scroll_factor 1
+  hyprctl keyword input:scroll_factor 1
 fi
